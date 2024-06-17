43874f46c17c162b91d31deec43d70117324a74a
AraceliDC <agdc.105@gmail.com>
2024-06-16 23:23:31 -0400

    7

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,14 @@
-import React from "react";
+// import React from "react";
 
-const Card = ({title, image, description})=>{
+// const Card = ({title, image, description})=>{
 
-    return(
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img className="w-full" src={image} alt={title} />
-        <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{title}</div>
-            <p className="text-gray-700 text-base">{description}</p>
-        </div>
-    </div>
-    )
-}
+//     return(
+//         <div className="max-w-sm rounded overflow-hidden shadow-lg">
+//         <img className="w-full" src={image} alt={title} />
+//         <div className="px-6 py-4">
+//             <div className="font-bold text-xl mb-2">{title}</div>
+//             <p className="text-gray-700 text-base">{description}</p>
+//         </div>
+//     </div>
+//     )
+// }
